ce42697ef532be41dbc9f895024bce45b3a46c88
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2022-06-21 17:47:56 +0200

    Add files via upload

--- detection/models_import.py
+++ detection/models_import.py
@@ -3,6 +3,8 @@ import numpy as np, pylab as plt
 import cv2, dlib
 import os, argparse
 
+from utils_math import pseudo_square
+
 ## Input
 parser = argparse.ArgumentParser(description='Animate an image understanding by a neural network - App')
 parser.add_argument('-i', '--input', type=str, required=False, help='directory of input known faces')
@@ -11,9 +13,13 @@ parser.add_argument('-i', '--input', type=str, required=False, help='directory o
 MODEL_DIR = 'saved_models/'
 INPUT_DIR = 'input/'
 
+## global param
+DEFAULT_CONFIANCE = 0.15
+THRESHOLD = 0.1
+
 def detection_model(MODEL_DIR):
-	print("[INFO] Importing pretrained YOLO model with COCO dataset and five scales architechtre network (Darknet)..")
-	bbox_detection = cv2.dnn.readNet(MODEL_DIR+'yolo/yolov4.cfg', MODEL_DIR+'yolo/yolov4.weights') # Bounding box
+	print("[INFO] Importing pretrained YOLO model with COCO dataset (Tiny Darknet)..")
+	bbox_detection = cv2.dnn.readNet(MODEL_DIR+'yolo/yolov3-tiny_thresh0.cfg', MODEL_DIR+'yolo/yolov3-tiny.weights') # Bounding box
 	print("[INFO] Importing frontal face detector model (HOG+SVM)..")
 	face_detector = dlib.get_frontal_face_detector()
 	print("[INFO] Importing landmarks detector model (dataset ?)..")
@@ -24,14 +30,76 @@ def detection_model(MODEL_DIR):
 	return bbox_detection, face_detector, pose_predictor_68_point, face_encoder
 
 def bounding_box_yolo(image,model,param):
+	image_bb = image.copy()
 	# parameter
 	ln, labels = param
-	# apply (adapt before image for net)
+	height, width, _ = image.shape
+	# apply (adapt before image for net & we have 2 detection layers for this networks)
 	blob = cv2.dnn.blobFromImage(image, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
 	model.setInput(blob)
 	layerOutputs = model.forward(ln)
-	# object detection (we have 3 detection layers for this networks)
+	
+	# initialize our lists of detected bounding boxes, confidences, and class IDs, respectively
+	grids, boxes, confidences, classIDs = [], [], [], []
 
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		layer = output[:,5:]
+		#layer[layer < 0.25] = 0.
+		lbl_idx = np.argmax(layer,axis=1)
+		grid = pseudo_square(lbl_idx).astype('uint8').T
+		grids += [cv2.resize(grid, (height, width), interpolation = cv2.INTER_AREA).T]
+		# loop over each of the detections
+		for detection in output:
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+			# select & scale
+			if confidence > DEFAULT_CONFIANCE:
+				box = detection[0:4] * np.array([width, height, width, height])
+				(centerX, centerY, W, H) = box.astype("int")
+				x = int(centerX - (W / 2))
+				y = int(centerY - (H / 2))
+				# update
+				boxes.append([x, y, int(W), int(H)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping bounding box
+	indexes = cv2.dnn.NMSBoxes(boxes, confidences, DEFAULT_CONFIANCE, THRESHOLD)
+	# initialize a list of colors to represent each possible class label
+	COLORS = 255*plt.cm.get_cmap('viridis',len(labels)).colors[:,:3]
+
+	# ensure at least one detection exists
+	if len(indexes) > 0:
+		# loop over the indexes we are keeping
+		for i in indexes.flatten():
+			# extract the bounding box coordinates
+			(x, y, w, h) = boxes[i]
+			# draw a bounding box rectangle and label on the frame
+			color = COLORS[classIDs[i]]
+			text = "{}: {:.4f}".format(labels[classIDs[i]], confidences[i])
+			cv2.rectangle(image_bb, (x, y), (x + w, y + h), color, 2)
+			cv2.putText(image_bb, text, (x, y + 20 ), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+	
+	return image_bb, grids
+
+def landmark_detection(frame, face_detector, pose_predictor_68_point):
+	rgb_small_frame = frame[:, :, ::-1]
+	face_locations = face_detector(rgb_small_frame, 1)
+	shapes, landmarks_list = [],[]
+	# face detection loop with landmark
+	for d in face_locations :
+		cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), 255, 1)
+		shape = pose_predictor_68_point(frame, d)
+		shapes += [shape]
+		shape = [(i.x, i.y) for i in shape.parts()]
+		landmarks_list += [shape]
+	# add landmarks
+	for shape in landmarks_list:
+			for (x, y) in shape:
+				cv2.circle(frame, (x, y), 1, (255, 0, 255), -1)
+	return landmarks_list, shapes
 
 if __name__ == '__main__':
 	print("[INFO] Starting System...")
@@ -41,7 +109,6 @@ if __name__ == '__main__':
 
 	print('[INFO] Prepare model..')
 	layer_name_output = [bbox_detection.getLayerNames()[i - 1] for i in bbox_detection.getUnconnectedOutLayers()]
-	output_before_relu = [bbox_detection.getLayerNames()[i - 3] for i in bbox_detection.getUnconnectedOutLayers()]
 	print('[INFO] Importing coco class labels..')
 	with open(MODEL_DIR + 'yolo/coco.names', 'r') as f:
 		LABELS = f.read().splitlines()
@@ -57,21 +124,23 @@ if __name__ == '__main__':
 	print('[INFO] Detecting...')
 	while True:
 		ret, frame = video_capture.read()
-		cv2.imshow('Input camera', frame)
-		obj_img = frame
-		blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
-		bbox_detection.setInput(blob)
-		layerOutputs = bbox_detection.forward(layer_name_output)
-		Outputs_before = bbox_detection.forward(output_before_relu)
-		conv_max = np.argmax(Outputs_before[1][0],axis=0).astype('uint8')
-		img = cv2.resize(conv_max, frame.shape[:2], interpolation = cv2.INTER_AREA).T
-		img_norm_8U = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-		img_rgb = cv2.cvtColor(img_norm_8U,cv2.COLOR_GRAY2RGB)
-		dst = cv2.addWeighted(frame,1.0,img_rgb,0.8,0)
-		cv2.imshow('Object detection i', dst) #obj_img)
+		#cv2.imshow('Input camera', frame)
+		## YOLO
+		img_bb, grids = bounding_box_yolo(frame,bbox_detection,(layer_name_output, LABELS))
 		"""
-		cv2.imshow('Frontal face detection (i,j)', frame)
-		cv2.imshow('Landmarks detection (i,j)', frame)
+		dst = cv2.addWeighted(frame,1.0,img_rgb,0.8,0)
+		"""
+		cv2.imshow('Object detection i', img_bb) # dst) #obj_img)
+		## FACE
+		landmarks_list, shapes = landmark_detection(frame, face_detector, pose_predictor_68_point)
+		cv2.imshow('Landmarks detection with frontal face detection (i,j)', frame)
+		face_encodings_list = []
+		for s in shapes :
+			face_encodings_list.append(np.array(face_encoder.compute_face_descriptor(frame, s, num_jitters=1)))
+		for face_encoding in face_encodings_list:
+			#vectors = np.linalg.norm(known_face_encodings - face_encoding, axis=1)
+			pass
+		"""
 		cv2.imshow('Face recognition (i,k)', frame)
 		cv2.imshow('Composite', frame)
 		"""
